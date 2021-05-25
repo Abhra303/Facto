72bd4339b523bc58ec221d80217b1fab3dc493b7
Abhra303 <chakrabortyabhradeep79@gmail.com>
2021-05-25 14:54:04 +0530

    improve UI for the application

---
+++ src/components/Loading/LoadingResult.jsx
@@ -0,0 +1,10 @@
+import './Loading.scss';
+import React from 'react';
+
+export default class LoadingResult extends React.Component {
+    render() {
+        return (
+            <div id="loader"></div>
+        )
+    }
+}
